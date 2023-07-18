25ad3a37e51a005eef4a979bf8e0cfc683e8b27b
Naglis Bagdonas <naglissbagdonass@gmail.com>
2023-07-18 15:20:02 +0300

    juvenile prevention

--- js/main.js
+++ js/main.js
@@ -6,3 +6,15 @@ export function sum(a, b, c) {
 
 const result = sum(10, 20, 40);
 console.log('result ===', result);
+
+// ========================
+
+function isEligible(num) {
+  if (num > 18) {
+    console.log('Welcome');
+  } else {
+    console.log('YOU ARE TOO YOUNG TO ACCESS');
+  }
+}
+
+isEligible(14);
